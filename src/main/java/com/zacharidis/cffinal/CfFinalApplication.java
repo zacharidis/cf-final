640b2b6ad673af2920f04165ff5cc28d23be6c2b
Georgios Zacharidis <zacharidis@icloud.com>
2022-09-24 22:08:50 +0300

    Initial commit

---
+++ src/main/java/com/zacharidis/cffinal/CfFinalApplication.java
@@ -0,0 +1,13 @@
+package com.zacharidis.cffinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zacharidis/cffinal/CfFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacharidis.cffinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
