cad8a765208ccc6a61f04c1ef76dae1165e2ba12
Georgios Zacharidis <zacharidis@icloud.com>
2022-09-24 22:25:48 +0300

    Subject entity created

---
+++ src/main/java/com/zacharidis/cffinal/entities/Subject.java
@@ -0,0 +1,60 @@
+package com.zacharidis.cffinal.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long subjectId;
+    private String name;
+    private String stage; // finished , hasn't started , ongoing
+    private String Description;
+
+    // Constructors
+
+    public Subject() {}
+
+    public Subject(String name, String stage, String description) {
+        this.name = name;
+        this.stage = stage;
+        Description = description;
+    }
+
+
+    // Getters and Setters
+
+    public long getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(long subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
